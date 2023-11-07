8730ce1fbc36fd6911e9ace92945bc3404dafb62
Elias Gomes <eliasgo221@gmail.com>
2023-11-07 16:58:09 -0300

    customizando Checkout page

--- src/pages/Checkout/index.tsx
+++ src/pages/Checkout/index.tsx
@@ -1,9 +1,13 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Card from '../../components/Card'
-import { InputGroup, Row } from './styles'
+import { InputGroup, Row, TabButton } from './styles'
 import Button from '../../components/Button'
+import boleto from '../../assets/images/boleto.png'
+import card from '../../assets/images/credit-card.png'
 
 const Checkout = () => {
+  const [payWithCard, setPayWithCard] = useState(false)
+
   return (
     <div className="container">
       <Card title="Dados de cobrança">
@@ -36,15 +40,84 @@ const Checkout = () => {
         </>
       </Card>
       <Card title="Pagamento">
-        <div>
-          <p>
-            Ao optar por essa forma de pagamento, é importante lembrar que a
-            confirmação pode levar até 3 dias úteis, devido aos prazos
-            estabelecidos pelas instituições financeiras. Portanto, a liberação
-            do código de ativação do jogo adquirido ocorrerá somente após a
-            aprovação do pagamento do boleto.
-          </p>
-        </div>
+        <>
+          <TabButton
+            isActive={!payWithCard}
+            type="button"
+            title="Cartão de crédito"
+            onClick={() => setPayWithCard(false)}
+          >
+            <img src={boleto} alt="Boleto" />
+            Boleto bancário
+          </TabButton>
+          <TabButton
+            isActive={payWithCard}
+            type="button"
+            title="Cartão de crédito"
+            onClick={() => setPayWithCard(true)}
+          >
+            <img src={card} alt="Boleto" />
+            Cartão e crédito
+          </TabButton>
+          <div className="margin-top">
+            {payWithCard ? (
+              <>
+                <Row>
+                  <InputGroup>
+                    <label htmlFor="carOwner">Nome do titular do cartão</label>
+                    <input id="text" type="carOwner" />
+                  </InputGroup>
+                  <InputGroup>
+                    <label htmlFor="carOwner">CPF do titular do cartão</label>
+                    <input id="carOwner" type="text" />
+                  </InputGroup>
+                </Row>
+                <Row marginTop="24px">
+                  <InputGroup>
+                    <label htmlFor="cardDisplayName">Nome no cartão</label>
+                    <input id="cardDisplayName" type="text" />
+                  </InputGroup>
+                  <InputGroup>
+                    <label htmlFor="cardNumber">Número do cartão</label>
+                    <input id="cardNumber" type="number" />
+                  </InputGroup>
+                  <InputGroup maxWidth="125px">
+                    <label htmlFor="expiresMonth">Mês De Vencimento</label>
+                    <input id="expiresMonth" type="text" />
+                  </InputGroup>
+                  <InputGroup maxWidth="123px">
+                    <label htmlFor="expireYear">Ano De Vencimento</label>
+                    <input id="expireYear" type="text" />
+                  </InputGroup>
+                  <InputGroup maxWidth="48px">
+                    <label htmlFor="cardCode">CVV</label>
+                    <input id="cardCode" type="number" />
+                  </InputGroup>
+                </Row>
+                <Row marginTop="24px">
+                  <InputGroup maxWidth="150px">
+                    <label htmlFor="installments">Parcelamento</label>
+                    <select>
+                      <option>1 x de R$ 200,00</option>
+                      <option>1 x de R$ 200,00</option>
+                      <option>1 x de R$ 200,00</option>
+                    </select>
+                  </InputGroup>
+                </Row>
+              </>
+            ) : (
+              <>
+                <p>
+                  Ao optar por essa forma de pagamento, é importante lembrar que
+                  a confirmação pode levar até 3 dias úteis, devido aos prazos
+                  estabelecidos pelas instituições financeiras. Portanto, a
+                  liberação do código de ativação do jogo adquirido ocorrerá
+                  somente após a aprovação do pagamento do boleto.
+                </p>
+              </>
+            )}
+          </div>
+        </>
       </Card>
       <Button type="button" title="Cliquer aqui para finalizar a compra">
         Finalizar a compra
--- src/pages/Checkout/styles.ts
+++ src/pages/Checkout/styles.ts
@@ -1,22 +1,53 @@
 import styled from 'styled-components'
 import { colors } from '../../styles'
 
-export const Row = styled.div`
+type InputGroupProps = {
+  maxWidth?: string
+  marginTop?: string
+}
+
+type TabButtonProps = {
+  isActive: boolean
+}
+
+export const Row = styled.div<InputGroupProps>`
   display: flex;
   column-gap: 24px;
+  margin-top: ${(props) => props.marginTop || '0'};
+  align-items: flex-end;
 `
-export const InputGroup = styled.div`
+export const InputGroup = styled.div<InputGroupProps>`
   flex: auto;
+  max-width: ${(props) => props.maxWidth || 'auto'};
   label {
     font-size: 14px;
     margin-bottom: 8px;
     display: block;
   }
-  input {
+
+  input,
+  select {
     width: 100%;
     background-color: ${colors.white};
     height: 32px;
     padding: 0 8px;
     border: 1px solid ${colors.white};
+  }
+`
+
+export const TabButton = styled.button<TabButtonProps>`
+  border-radius: 8px;
+  font-size: 14px;
+  font-weight: bold;
+  color: ${colors.white};
+  background-color: ${(props) =>
+    props.isActive ? colors.green : colors.black};
+  height: 32px;
+  border: none;
+  margin-right: 16px;
+  padding: 0 8px;
+  cursor: pointer;
+  img {
+    margin-right: 8px;
   }
 `
